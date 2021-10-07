5dd2a449ea9e4d5e7a8335fde4145ab258aa04d7
Ernesto Estrada <erestrada14@gmail.com>
2021-10-07 12:14:53 -0500

    Add data to genarate message

---
+++ script.js
@@ -0,0 +1,7 @@
+// This is a random message generator, it prints a new massage every time the user runs this script.
+
+const greet = ['Hello', 'Hey', 'Hi', 'Hoy', 'Hello there', 'Howdy', 'Hey y\'all'];
+const question = ['How are you', 'How are ya', 'How are things', 'How\'s it going', 'What\'s happening'];
+const answer = ['I hope you\'re great', 'I hope you\'re staying healthy', 'I hope you are having a productive day', 'I miss you! Are you doing okay?'];
+const str = 'I just wanted to say';
+const wish = ['Write it on your heart that every day is the best day in the year.', 'This is a wonderful day; I have never seen this one before', 'A great attitude is like a perfect cup of coffee – don’t start your day without it.', ' Make each day your masterpiece.', 'A good day is a good day. A bad day is a good story.', 'A bad day for our ego is a great day for our soul.'];
